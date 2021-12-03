18d931848bebe6e7f01d0543e96e7aaef9b08014
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-03 09:02:39 -0600

    Adding support for build caches for scala

--- run.sh
+++ run.sh
@@ -41,10 +41,44 @@ main() {
   )
 }
 
+CACHE_DIR='.cp_cache'
+# Cache handling
+cache_dir_for() {
+  FILE=$1
+
+  HASH=$(sha256sum "$FILE" | cut -d' ' -f1)
+  TARGET_DIR="$CACHE_DIR/$HASH"
+
+  printf '%s' "$TARGET_DIR"
+  if [[ -d "$TARGET_DIR" ]]; then
+    return 0
+  else
+    mkdir -p "$TARGET_DIR"
+    return 1
+  fi
+}
+
 # Extension handlers
 hs() { runghc "$@"; }
 py() { python "$@"; }
-_scala() { scala "$@"; }
+
+build_scala() {
+  FILE_NAME=$1
+
+  if TARGET_DIR=$(cache_dir_for "$FILE_NAME"); then
+    cd "$TARGET_DIR"
+  else
+    cp "$FILE_NAME" "$TARGET_DIR"
+    cd "$TARGET_DIR"
+    scalac "$FILE_NAME"
+  fi
+}
+
+_scala() {
+  FILE_NAME=$1
+  build_scala "$FILE_NAME"
+  scala main "${@:2}";
+}
 
 main "$@"
 
