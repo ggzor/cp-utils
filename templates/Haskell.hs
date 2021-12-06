b3e8bbaf13d9b9a0277c4a5d45ec44554208b125
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-06 11:31:26 -0600

    Updating Haskell template

--- templates/Haskell.hs
+++ templates/Haskell.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
-import Data.Bifunctor (first, second)
+import Control.Applicative
+import Control.Monad
+import Data.Bifunctor
 import Data.Char as C
 import Data.Function
+import Data.Functor
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Ord
+import Data.Tuple
 
 import System.Environment
 
