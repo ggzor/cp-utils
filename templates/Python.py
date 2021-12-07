8759e19341e1be927e31165a1d10a83de518f8e2
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-07 09:20:37 -0600

    Updating Python template

--- templates/Python.py
+++ templates/Python.py
@@ -1,3 +1,8 @@
 from sys import argv, stdin
 import itertools as it
 import operator as op
+from collections import Counter, defaultdict
+
+nums = [*map(int, stdin.read().split(","))]
+
+print(dict(list(it.dropwhile(lambda t: t[0] != "defaultdict", locals().items()))[1:]))
