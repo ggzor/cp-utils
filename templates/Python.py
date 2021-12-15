14c7de9e22163d8ee53edc51c1e9f9b77bcd1a83
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-15 09:01:40 -0600

    Updating Python template

--- templates/Python.py
+++ templates/Python.py
@@ -1,8 +1,21 @@
 from sys import argv, stdin
 import itertools as it
 import operator as op
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, deque
+from queue import PriorityQueue
+from pprint import PrettyPrinter
 
-nums = [*map(int, stdin.read().split(","))]
+pprint = PrettyPrinter(indent=2, compact=True, sort_dicts=False).pprint
 
-print(dict(list(it.dropwhile(lambda t: t[0] != "defaultdict", locals().items()))[1:]))
+text = stdin.read()
+lines = text.splitlines()
+
+xs = [[int(v) for v in l] for l in lines]
+h = len(xs)
+w = len(xs[0])
+# print("\n".join("".join(map(str, r)) for r in xs))
+
+xs = {(y, x): int(v) for y, l in enumerate(lines) for x, v in enumerate(l)}
+w = max(y for y, _ in xs.keys()) + 1
+h = max(x for _, x in xs.keys()) + 1
+# print("\n".join("".join(str(xs[(y, x)]) for x in range(w)) for y in range(h)))
